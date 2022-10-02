9991267c5e643b0406277cc93971eff003585c9d
Zachary Wells <zwucsc@gmail.com>
2022-10-02 15:35:27 -0700

    needed to use prepended timestamp in an app

--- IO/Output.py
+++ IO/Output.py
@@ -2,10 +2,18 @@ import time
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
+def getTimeStampedPrependedFile(outputFile):
+
+    return time.strftime("%Y%m%d-%H%M%S--") + outputFile
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 def saveDictToNewTimeStampedFile(outputFile, outputDict):
 
-    outputFile = time.strftime("%Y%m%d-%H%M%S--") + outputFile
+    outputFile = getTimeStampedPrependedFile(outputFile)
     f = open(outputFile, "a")
     for key, value in outputDict.items():
         f.write('file:  %s:     Strings Found:  %s\n' % (key, value))
     f.close()
+
+
